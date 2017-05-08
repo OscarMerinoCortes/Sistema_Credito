4c9e2ef65c822fead5658fe8cf0d6049d300885e
mikeCq <miguelcarrilloqnz@hotmail.com>
2017-05-08 15:40:18 -0600

    sp

--- Scripts/Procedimientos/sp_LlenarComboClientes.sql
+++ Scripts/Procedimientos/sp_LlenarComboClientes.sql
@@ -1,8 +1,12 @@
-CREATE procedure sp_LlenarComboClientes
+alter procedure sp_LlenarComboClientes
 --declare
 as
 select 
 	   IdCliente,
-	   Nombre+''+SegundoNombre+' '+ApellidoPaterno+' '+ ApellidoMaterno as Nombre
+	   Nombre+	   CASE SegundoNombre
+					 when '' then SegundoNombre
+					 else ' '+SegundoNombre+' '
+				   end
+	   +ApellidoPaterno+' '+ ApellidoMaterno as Nombre
 	   from   [dbo].[Cre_Preregistro_Cliente_Encabezado] a
 where  a.IdEstatus = 1
